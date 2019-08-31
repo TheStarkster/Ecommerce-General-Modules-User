19205d80ef535ce71ea497327b30813c038e4d49
Unknown <sgurkaran2000@gmail.com>
2019-08-31 11:49:30 +0530

    Commit

--- client/egm/src/components/user/ProductGrid.js
+++ client/egm/src/components/user/ProductGrid.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+// import { Container, Row, Col } from 'react-bootstrap'
 import ProductsCard from './ProductsCard'
 
 class ProductGrid extends Component {
     render() {
         return (
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm ">
-                        <ProductsCard></ProductsCard>
-                    </div>
-                    <div class="col-sm">
-                        <ProductsCard></ProductsCard>
-                    </div>
-                    <div class="col-sm">
-                        <ProductsCard></ProductsCard>
-                    </div>
-                    <div class="col-sm">
-                        <ProductsCard></ProductsCard>
+            <div className="container-fluid">
+                <div className="row my-4">
+                    <div className="col-lg-12">
+                        <div className="d-flex flex-wrap justify-content-around">
+                            <ProductsCard></ProductsCard>
+                            <ProductsCard></ProductsCard>
+                            <ProductsCard></ProductsCard>
+                            <ProductsCard></ProductsCard>
+                        </div>
                     </div>
                 </div>
             </div>
