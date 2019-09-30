58740f392021964f789d9a15d5b1fb0661ccf2b3
Gurkaran Singh <sgurkaran2000@gmail.com>
2019-09-30 15:52:55 +0530

    Minor Changes in Home

--- User/client/egm/src/components/Home.js
+++ User/client/egm/src/components/Home.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react'
 import CustomNav from './user/master-components/Navbar'
 import ReactCarousel from './user/master-components/Carousel'
 // import Tags from './user/master-components/tags'
+import $ from 'jquery'
 import SearchBar from './user/master-components/searchbar'
 import ProductGrid from './user/ProductGrid'
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            RenderCarousel:false
+        }
+        this.ChangeCarouselState = () => {
+            this.setState({
+                RenderCarousel:true
+            })
+        }
+        this.componentDidMount = () => {
+            var that = this
+            $(window).trigger('load')
+            $(window).on('load',function(){
+                var win = $(window)
+                if(win.width() >= 425){
+                    that.ChangeCarouselState()                    
+                }
+            })
+            
+        }
+    }
     render() {
         return (
             <div>
                 <CustomNav history={this.props.history} loggedIn={false}></CustomNav>
-                <ReactCarousel></ReactCarousel>
+                {/* <ReactCarousel></ReactCarousel> */}
+                { this.state.RenderCarousel ? <ReactCarousel /> : null}
                 <SearchBar></SearchBar>
                 {/* <Tags></Tags> */}
                 <ProductGrid history={this.props.history}></ProductGrid>
--- User/client/egm/src/components/user/master-components/Carousel.js
+++ User/client/egm/src/components/user/master-components/Carousel.js
@@ -8,21 +8,21 @@ class ReactCarousel extends Component {
             <Carousel>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="w-100"
                         src={require('../../../components/assets/images/Carousel/1.jpg')}
                         alt="First slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="w-100"
                         src={require('../../../components/assets/images/Carousel/1.jpg')}
                         alt="Third slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
+                        className="w-100"
                         src={require('../../../components/assets/images/Carousel/1.jpg')}
                         alt="Third slide"
                     />
